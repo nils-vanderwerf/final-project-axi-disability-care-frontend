d09a99da01d6499790a0398e8f0eb1beabf797ae
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-07-15 17:01:32 +1000

    Added sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-    has_secure_password
+    # password will be salted, wants a password and password_confirmation
+    has_secure_password 
+
+    validates_presence_of :email
+    validates_uniqueness_of :email
 end
