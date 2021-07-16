508ac4a6ecfe9619d8a61f1e25d64a148a69063a
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-07-16 12:40:47 +1000

    Committed to main

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,31 +1,31 @@
 class Api::V1::SessionsController < ApplicationController
     include CurrentUserConcern
         def create
-            user = User
-                    .find_by(email: params["user"]["email"])
-                    .try(:authenticate, params["user"]["password"])
-        if user 
-            session[:user_id] = user.id
-            byebug
-            render json: {
-                status: :created,
-                logged_in: true
-                user: user
-            }
+                user = User
+                        .find_by(email: params["user"]["email"])
+                        .try(:authenticate, params["user"]["password"])
+                        
+            if user  
+                session[:user_id] = user.id
+                render json: {
+                    status: :created,
+                    logged_in: true,
+                    user: user
+                }
+            else
+            end 
+                render json: [status: 401]
+            end
         end
-        else 
-            render json: [status: 401]
-            flash.now[:danger]="Email/Password is not matching"
-        end
-    
+       
         def logged_in
             if @current_user 
                 render json: {
-                    logged_in: true
+                    logged_in: true,
                     user: @current_user
                 }
             else 
-                render: {
+                render json: {
                     logged_in: false
                 }
             end
@@ -33,6 +33,6 @@ class Api::V1::SessionsController < ApplicationController
     
         def logout 
             reset_session
-            render json {status: 200, logged_out: true}
+            render json: {status: 200, logged_out: true}
         end
     end
