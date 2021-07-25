4adf5ec9a2ef706ccee881653cf747c71945cb80
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-07-25 14:52:51 +1000

    Showing user profile

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,27 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import axios from 'axios'
 import Registration from './auth/Registration/Registration'
 import LoginForm from './auth/loginForm'
 import CategoryList from './Categories/CategoryList'
-import { AppBar, Container, Button, Grid } from '@material-ui/core';
+import { AppBar, Container, Button, Grid, TextField } from '@material-ui/core';
+import { Search, GpsFixed } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
 import Carers from './User/CarersContainer'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng,
+} from 'react-places-autocomplete';
 
-export default class Home extends Component {
 
+class Home extends Component {
     constructor(props) {
-        super(props);
-        // this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        super(props)
+
+        this.handleChange = this.handleChange.bind(this)
     }
 
+    handleChange = address => {
+        this.setState({ address });
+    };
+
+    handleSelect = address => {
+        geocodeByAddress(address)
+            .then(results => getLatLng(results[0]))
+            .then(latLng => console.log('Success', latLng))
+            .catch(error => console.error('Error', error));
+    };
 
 
     render() {
         return (
-            <div className="login-hero-container">
-                <CategoryList/>
-            </div>
+            <Container>
+                <CategoryList />
+            </Container>
         )
     }
 }
+
+export default Home;
--- src/components/pages/User/IndividualCarer.js
+++ src/components/pages/User/IndividualCarer.js
@@ -12,13 +12,27 @@ class IndividualCarer extends Component {
   }
     render() {
         const carers = this.props.userData.carers.users
-        return (
-            <div>
-                {/* {console.log("List of carers from variable", carers)} */}
-                {/* {console.log("Individual user",  this.props.userData)} */}
-                {carers.find(user => console.log("Params match", this.props.match.params))}
-            </div>
-        )
+        console.log("Carers", carers)
+        console.log("Params match: ", this.props.match.match.params.id)
+        const matchId = parseInt(this.props.match.match.params.id)
+        const thisCarer = carers.find(carer => carer.id === matchId)
+        if (this.props.userData && 
+          this.props.userData.carers && 
+          this.props.userData.carers.users && 
+          this.props.userData.carers.users.length > 0) {
+            const thisUser = carers.filter(user => user.id == this.props.match.match.params.id)
+          return (
+              <div>
+                {/* {console.log("Props id:", this.props.match.match.params.id)} */}
+                <h1>{thisCarer.first_name} {thisCarer.last_name}</h1>
+                <h2>Hourly rate: ${thisCarer.hourly_rate}</h2>
+                <p><strong>Bio:</strong> {thisCarer.bio}</p>
+                 {/* {this.props.userData.carers.users.find(user => user.id === this.props.match.params.id)} */}
+              </div>
+          )
+        } else {
+          return <div> <h1> Loading </h1> </div>
+        }
     }
 }
 
